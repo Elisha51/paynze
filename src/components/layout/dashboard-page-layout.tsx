f2e95ca9dcc6b555451fcb966a18593af1e1c975
Nampwera Elisha <nampweraelisha34@gmail.com>
2025-10-25 14:40:04 +0000

    Try fixing this error: `Runtime Error: Error: CardHeader is not defined.

--- src/components/layout/dashboard-page-layout.tsx
+++ src/components/layout/dashboard-page-layout.tsx
@@ -3,6 +3,7 @@
 import {
   Card,
   CardContent,
+  CardHeader,
 } from '@/components/ui/card';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useSearch } from '@/context/search-context';
